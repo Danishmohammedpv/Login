1ccc1b3267a9ba257997c0c8c88e9352b7636188
DANI0376 <danishmohammed.rocket@gmail.com>
2023-01-23 12:29:05 +0530

    alliswell

--- lib/homae_page.dart
+++ lib/homae_page.dart
@@ -1,5 +1,8 @@
 // import 'package:firebase2/Screens/login.dart';
 // import 'package:firebase2/services/firebase_services.dart';
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter/src/widgets/container.dart';
@@ -15,34 +18,73 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController noteController = TextEditingController();
+  User? userid = FirebaseAuth.instance.currentUser;
+  // final _controller = TextEditingController();
+
+  // Future<void> _saveTask() async {
+  //   final taskName = _controller.text;
+  //   await FirebaseFirestore.instance
+  //       .collection("Tasks")
+  //       .add({"name": _saveTask});
+  //   _controller.clear();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.network(FirebaseAuth.instance.currentUser!.photoURL!),
-              SizedBox(
-                height: 20,
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(FirebaseAuth.instance.currentUser!.photoURL!),
+            SizedBox(
+              height: 20,
+            ),
+            Text("${FirebaseAuth.instance.currentUser!.displayName}"),
+            Text("${FirebaseAuth.instance.currentUser!.email}"),
+            ElevatedButton(
+              onPressed: () async {
+                await FirebaseServices().signOut();
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const Loginscreen()));
+              },
+              child: const Text(
+                'Logout',
               ),
-              Text("${FirebaseAuth.instance.currentUser!.displayName}"),
-              Text("${FirebaseAuth.instance.currentUser!.email}"),
-              ElevatedButton(
-                onPressed: () async {
-                  await FirebaseServices().signOut();
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const Loginscreen()));
-                },
-                child: const Text(
-                  'Logout',
-                ),
-              ),
-            ],
-          ),
-        ));
+            ),
+            Expanded(
+                child: TextFormField(
+              maxLines: null,
+              controller: noteController,
+              // controller: _controller,
+              decoration: InputDecoration(hintText: "Enter tasks"),
+            )),
+            TextButton(
+                onPressed: (() async {
+                  var note = noteController.text.trim();
+                  if (note != "") {
+                    try {
+                      await FirebaseFirestore.instance
+                          .collection("notes")
+                          .doc()
+                          .set({
+                        "createdAt": DateTime.now(),
+                        "note": note,
+                        "userid": userid?.uid,
+                      });
+                    } catch (e) {
+                      print("error $e");
+                    }
+                  }
+
+                  // await _saveTask();
+                }),
+                child: Text('Save')),
+          ],
+        )));
   }
 }
